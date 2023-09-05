a47c719d744bf03fc4c8aaedb9d67b88b84597f1
emman0000 <142867499+emman0000@users.noreply.github.com>
2023-09-05 10:46:41 +0500

    Add files via upload

---
+++ test01.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int r, h;
+float Pi=3.14, Volume=0;
+Pi=3.14;
+printf("Enter Value\n");
+printf("Enter Value\n");
+
+
+scanf("%d",&r);
+scanf("%d",&h);
+
+
+volume=Pi*r*r*h*;
+printf("%f", volume);
+
+
+return volume;
+}
---
+++ test02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+ int= base, hyp=0, perp;
+printf("enter the base");
+scanf("%d",& base);
+printf("enter the perp");
+scanf("%d",& perp);
+
+hyp=sqrt(base*base+perp*perp);
+scanf("%d",&hyp);
+printf("%d",hyp);
+return hyp;
+ 
+ 
+
+}
