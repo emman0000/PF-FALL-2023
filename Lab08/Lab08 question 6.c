92d1fb1b23c59238a7f2171bb3aaef60c06d1f08
emman0000 <142867499+emman0000@users.noreply.github.com>
2023-10-24 22:34:37 +0500

    Update Lab08 question 6.c

--- Lab08/Lab08 question 6.c
+++ Lab08/Lab08 question 6.c
@@ -1,10 +1,16 @@
+// Programmer Emman Abrar
+//This program uses an array and takes an input for the size of the array
+//It prints the highest value in the array
+//It prints the lowest value in the array
+//It prints the sum of all the numbers included in the array
+//24/10/23
 #include<stdio.h>
-
+//Using a fucntion to increment i and calculate sum of numbers in array
 void processArray(int size, int arr[]) {
 int i, sum=0;
 for(i=0; i<size; i++) {
 sum=sum+arr[i];
-}
+}//end void
 printf("The sum is: %d\n", sum);
 int max=arr[0];
 for(i=0; i<size; i++) {
@@ -12,15 +18,16 @@ if(max<arr[i]) {
 max=arr[i];
 }
 }
-printf("Maximum: %d\n", max);
-
+printf("Maximum value in the array is: %d\n", max);
+//declaring a variable to find the minimum value
+//increment i until is less than the size, if min is 
 int min=arr[0];
 for(i=0; i<size; i++) {
 if(min>arr[i]) {
 min=arr[i];
-}
-}
-printf("Minimum: %d\n", min);
+}//end if 
+}//end for
+printf("Minimum value in the array is: %d\n", min);
 }
 int main() {
 int i, size;
@@ -31,8 +38,10 @@ int arr[size];
 for(i=0; i<size; i++) {
 printf("Please enter %d element:", i+1);
 scanf("%d", &arr[i]);
-}
+}//end it 
+//recalling function in main
 processArray(size, arr);
 return 0;
-}
+}//end main
+
 
